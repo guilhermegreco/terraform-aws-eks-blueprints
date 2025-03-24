08b098231c630889f3a80f7fb704055eac880717
Guilherme Greco <guilhermegreco@hotmail.com>
2025-03-23 23:04:15 -0300

    added new repos

--- patterns/gitops/multi-cluster-hub-spoke-argocd/hub/variables.tf
+++ patterns/gitops/multi-cluster-hub-spoke-argocd/hub/variables.tf
@@ -6,7 +6,7 @@ variable "vpc_cidr" {
 variable "region" {
   description = "AWS region"
   type        = string
-  default     = "us-west-2"
+  default     = "us-east-1"
 }
 variable "kubernetes_version" {
   description = "Kubernetes version"
